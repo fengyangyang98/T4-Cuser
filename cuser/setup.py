321f2bccbdf1f53cc04ce0273e1e09a99dd16993
CynricFeng <37473925+CynricFeng@users.noreply.github.com>
2019-09-04 12:17:36 +0800

    serial

---
+++ cuser/setup.py
@@ -0,0 +1,12 @@
+# coding: utf-8
+
+from setuptools import setup
+
+setup(
+    name='cuser',
+    version='0.0.3',
+    author='fyy',
+    author_email='fengyangyang@hnu.edu.cn',
+    packages=['cuser'],
+    install_requires=['pyserial']
+)
